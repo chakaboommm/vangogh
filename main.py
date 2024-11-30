61003b95731cdb32f66f250e2ee8cc75ed566cb3
chakaboommm <qqa55bj6l2@hotmail.com>
2024-11-30 19:21:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import time
 import asyncio
 import os
 import random
-from datetime import datetime
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -42,16 +41,28 @@ async def main():
     print(f"My tweet ids: {my_tweet_ids}")
         
     
-    # sia_previous_posts = sia.memory.get_messages()
-    # print("Posts from memory:\n")
-    # for post in sia_previous_posts[-10:]:
-    #     print(post)
-    #     print("\n")
-    # print(f"{'*'*100}\n\n")
+    # sia_client = SiaTelegram(bot_token=os.getenv("TG_BOT_TOKEN"), chat_id="@real_sia")
+
+    sia_previous_posts = sia.memory.get_messages()
+    print("Posts from memory:\n")
+    for post in sia_previous_posts[-20:]:
+        print(post)
+        print("\n\n")
+    print(f"{'*'*100}\n\n")
 
 
+    # wait between 30 seconds and 15 minutes
+    #   before generating and publishing the next tweet
     times_of_day = sia.character.times_of_day()
+    # wait_time = random.randint(30, 900)
+    # wait_hours = wait_time // 3600
+    # wait_minutes = (wait_time % 3600) // 60
+    # wait_seconds = wait_time % 60
+    # print(f"\n\nWaiting for {wait_hours} hours, {wait_minutes} minutes, and {wait_seconds} seconds before generating and publishing next tweet.\n\n")
+    # time.sleep(wait_time)
     
+    tweeted = False
+
     start_time = time.time()
     
     # run for 45 minutes
@@ -59,18 +70,9 @@ async def main():
 
         character_settings = sia.memory.get_character_settings()
         
-        next_post_time = character_settings.character_settings.get('twitter', {}).get('next_post_time', 0)
-        next_post_datetime = datetime.fromtimestamp(next_post_time).strftime('%Y-%m-%d %H:%M:%S') if next_post_time else "N/A"
-        now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f"Current time: {now_time}")
-        next_post_time_seconds = next_post_time - time.time()
-        next_post_hours = next_post_time_seconds // 3600
-        next_post_minutes = (next_post_time_seconds % 3600) // 60
-        print(f"Next post time: {next_post_datetime} (posting in {next_post_hours}h {next_post_minutes}m)")
-        
         # posting
         #   new tweet
-        if time.time() > next_post_time:
+        if time.time() > character_settings.character_settings.get("twitter", {}).get("next_post_time", 0):
             # for now, for testing purposes we generate a tweet
             #   using a random time of day as context for AI,
             #   ignoring the actual time of the day
@@ -89,7 +91,7 @@ async def main():
 
                 character_settings.character_settings = {
                     "twitter": {
-                        "next_post_time": time.time() + sia.character.platform_settings.get("twitter", {}).get("post_frequency", 2) * 3600
+                        "next_post_time": time.time() + sia.character.platform_settings.get("twitter", {}).get("post_frequency", 2) * 1300
                     }
                 }
                 sia.memory.update_character_settings(character_settings)
@@ -98,32 +100,29 @@ async def main():
             time.sleep(30)
 
 
-        # replying
-        #   to new replies
+        # Check interactions
+        print("Checking for new interactions...")
         
-        print("Checking for new replies...")
-        replies = sia.twitter.get_new_replies_to_my_tweets()
-        if replies:
-            for r in replies:
-                print(f"Reply: {r}")
-                if r.flagged:
-                    print(f"Skipping flagged reply: {r}")
+        # Check mentions/notifications
+        notifications = sia.twitter.get_notifications()
+        if notifications:
+            for n in notifications:
+                print(f"Notification: {n}")
+                if n.flagged:
+                    print(f"Skipping flagged notification: {n}")
                     continue
-                generated_response = sia.generate_response(r)
-                if not generated_response:
-                    print(f"No response generated for reply: {r}")
-                    continue
+                generated_response, image_filepaths = sia.generate_response(n)
                 print(f"Generated response: {len(generated_response.content)} characters")
-                tweet_id = sia.twitter.publish_post(post=generated_response, in_reply_to_tweet_id=r.id)
+                tweet_id = sia.twitter.publish_post(post=generated_response, in_reply_to_tweet_id=n.id, media=image_filepaths)
                 if isinstance(tweet_id, Forbidden):
                     print(f"\n\nFailed to send reply: {tweet_id}. Sleeping for 10 minutes.\n\n")
                     time.sleep(600)
-                time.sleep(random.randint(20, 40))
-        else:
-            print("No new replies yet.")
-        print("\n\n")
+                time.sleep(random.randint(30, 60))
 
-        time.sleep(random.randint(20, 40))
+        if not notifications:
+            print("No new interactions yet.")
+        print("\n\n")
+        time.sleep(random.randint(30, 60))
 
 
 
